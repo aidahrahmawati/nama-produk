6ab91f7fd2fb3811ff3f764fe0331649c07942a9
aidahrahmawati <aidahrahmawati04@gmail.com>
2024-04-26 21:07:32 +0700

    mengnti main.js

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
-  authDomain: "insan-cemerlang.firebaseapp.com",
-  projectId: "insan-cemerlang",
-  storageBucket: "insan-cemerlang.appspot.com",
-  messagingSenderId: "579109661574",
-  appId: "1:579109661574:web:4a7cd4060f70eded945a07"
+  apiKey: "AIzaSyBNYqYJVYPTZRIK7pKZ4shbQ4igjseQpLM",
+  authDomain: "insan-cemerlang-d724d.firebaseapp.com",
+  projectId: "insan-cemerlang-d724d",
+  storageBucket: "insan-cemerlang-d724d.appspot.com",
+  messagingSenderId: "630693962922",
+  appId: "1:630693962922:web:a9447f760b858bcf781cd3"
 };
 
 // Inisialisasi Firebase
